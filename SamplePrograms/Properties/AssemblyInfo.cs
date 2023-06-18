7e57d153afead6b1733bd3d3c804d93d1620b768
SoDeRMond <SoDeRMond@gmail.com>
2023-06-18 13:03:59 +0300

    Corrected Version

--- SamplePrograms/Properties/AssemblyInfo.cs
+++ SamplePrograms/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Reflection;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("175ef175-61ed-4845-b4d5-852eb859523f")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
